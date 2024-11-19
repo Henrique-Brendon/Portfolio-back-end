d780220a03a3e1f7e082b292c944ff55d2dfc5f1
Henrique Brendon <henriquebrendonti26@gmail.com>
2024-11-19 15:20:39 -0300

    Entity Layer

---
+++ src/main/java/com/henrique/backend/entities/User.java
@@ -0,0 +1,95 @@
+package com.henrique.backend.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+public class User implements Serializable{
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String message;
+    private Instant messageDate;
+
+    public User(Long id, String name, String email, String phone, String message, Instant messageDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.message = message;
+        this.messageDate = messageDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Instant getMessageDate() {
+        return messageDate;
+    }
+
+    public void setMessageDate(Instant messageDate) {
+        this.messageDate = messageDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.id, other.id);
+    }
+  
+}
